94334dec8265ae6bab27520beb5a474d4bcd20cf
floompaloompa <79580225+floompaloompa@users.noreply.github.com>
2024-10-23 11:06:42 +0200

    Added enemy that follows

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+
+    [SerializeField] public GameObject Target;
+    public float Dis;
+    public float speed = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+
+        Dis = Vector2.Distance(transform.position, Target.transform.position);
+
+        transform.position = Vector2.MoveTowards(transform.position, Target.transform.position, Time.deltaTime);
+        
+
+        
+    }
+}
